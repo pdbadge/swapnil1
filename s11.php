ff1ad683969d561b28c4d419f04de5f9f992d8dd
Pooja Deepak Badge <poojadb1099@gmail.com>
2020-11-07 21:16:36 +0530

    add1

---
+++ s11.php
@@ -0,0 +1,9 @@
+<?php
+$products  = ["name" => "DEEPAK BADGE", "basic pay" => "55000", "description" => "smart tv", "size" => "24"];
+echo $products["name"];
+echo "<br>";
+echo $products["basic pay"];
+echo "<br>";
+echo $products["description"];
+echo "pooja";
+?>
