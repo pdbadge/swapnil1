f19056601ecb4e797acc1774ab0f1975bd409ca5
Pooja Deepak Badge <poojadb1099@gmail.com>
2020-11-10 12:04:10 +0530

    a11

---
+++ arrayfunctions.php
@@ -0,0 +1,16 @@
+<?php
+$a= Array(12 ,23 ,34 , 45 ,67, 78,);
+print_r(array_reverse($a));
+echo"<br>";
+echo"<br>";
+print_r(array_product($a));
+echo"<br>";
+echo"<br>";
+print_r(array_sum($a));
+echo"<br>";
+echo"<br>";
+print_r(count($a));
+echo"<br>";
+echo"<br>";
+print_r(array_chunk($a,2));
+?>
